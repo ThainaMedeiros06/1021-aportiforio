904e746147f3caf5e7b8a367b3efe2898a66ee75
ThainaMedeiros06 <thaina.santos4@estudante.ifms.edu.br>
2023-09-19 10:17:34 -0400

    aula dia 19/092023

--- src/componentes/portfolio/Portfolio.tsx
+++ src/componentes/portfolio/Portfolio.tsx
@@ -1,8 +1,43 @@
+import {useState} from 'react'
+import Projeto  from '../projeto/Projeto'
 import './Portfolio.css'
 import Pessoa from '../../assets/img/download-removebg-preview.png'
+type ProjetoType = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+
 
 function Portfolio(){
+    const [texto,setTexto]=useState("")
+
+    const projetos:ProjetoType[]= [
+        {
+            id:1,
+            titulo:'Voleibol NVC',
+            descricao:'knforehfpqhpi4rfhrphf',
+            imagem:''
+        },
+        {
+            id:2,
+            titulo:'Estágio',
+            descricao:'knforehfpqhpi4rfhrphf',
+            imagem:''
+        },
+        {
+            id:3,
+            titulo:'Site da Barbie',
+            descricao:'knforehfpqhpi4rfhrphf',
+            imagem:''
+        },
+    ]
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        setTexto(e.target.value)
+    }
     return(
+        <>
         <main>
             <div className='div-saibamais'>
                 <ul>
@@ -13,7 +48,7 @@ function Portfolio(){
                         <h1 className='Titulo1'>Me chamo Thainá Medeiros</h1>
                     </li>
                     <li>
-                        <p>Sejam bem-vidas ao meu portfolio.</p>
+                        <p>Sejam bem-vidos ao meu portfolio.</p>
                     </li>
                 </ul>
                 <button className='button-saibamais'>Saiba mais sobre mim</button>
@@ -22,6 +57,18 @@ function Portfolio(){
                 <img src={Pessoa}/>   
              </div>
         </main>
+        <div className='campo-pesquisa'>
+            <p>Pesquise um projeto</p>
+            <input type="text" className='input' placeholder='Pesquise um Projeto' onChange={TrataTexto} />
+            <img className='img-lupa' src="lupa.png" alt="" />
+            {
+                (texto)?<p>Resultados Para:{texto}</p>:""
+            }   
+            {
+                projetos.filter((projeto)=>projeto.titulo.toLocaleLowerCase().includes(texto)).map((projeto)=><Projeto key={projeto.id} titulo={projeto.titulo} descricao={projeto.descricao} imagem={projeto.imagem}/>)
+            }
+        </div>
+        </>
     )
 }
 export default Portfolio
